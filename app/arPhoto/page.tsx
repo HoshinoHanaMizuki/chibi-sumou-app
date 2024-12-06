2089624a9988d8cdb5ff1b89fa5274128fef5771
MasatomoIde <pakkuku1@gmail.com>
2024-12-07 02:30:26 +0900

    tryhiddenAndhandleCapture

--- app/arPhoto/page.tsx
+++ app/arPhoto/page.tsx
@@ -80,14 +80,51 @@ export default function ArPhoto() {
         drawFrame();
 
     },[]);
+    const handleCapture = () => {
+        if (canvasRef.current && videoRef.current) {
+            const canvas = canvasRef.current;
+            const context = canvas.getContext("2d");
+            if (context) {
+                // カメラ映像をキャンバスに描画
+                context.drawImage(videoRef.current, 0, 0, canvas.width, canvas.height);
 
+                // 最後にキャンバスを画像として保存
+                const dataUrl = canvas.toDataURL("image/png");
+                alert(`撮影された画像のデータURL: ${dataUrl}`);
+                // 画像を端末にダウンロード
+
+            }
+        }
+    };
     return (
         <>
             <div className="allContainer relative">
                 <NavBar />
                 {/* スマホの縦、横サイズのキャンバスを作成 */}
-                {/* <video ref={videoRef} autoPlay={true} playsInline={true} muted={true} /> */}
+                <div className="videoContainer hidden"><video ref={videoRef} autoPlay={true} playsInline={true} muted={true} /></div>
+                
                 <canvas ref={canvasRef} width={deviceSize.width} height={deviceSize.height} />
+                <div className="editingMenu fixed bottom-0 left-0 right-0 p-4 flex flex-col justify-center space-x-4">
+                    {/* キャラクター設定UIをここに追加 */}
+                    <div className="charactorSettingUI flex">
+                        {/* {brotherBirdImageList.map((image,index)=>(
+                            <Image alt="" key={index} src={image} width={deviceSize.width/7} height={70} onClick={()=>setCharaImage(image,setCurrentBroImage)} />
+                        ))}
+                        {sisterBirdImageList.map((image,index)=>(
+                            <Image alt="" key={index} src={image} width={deviceSize.width/7} height={70} onClick={()=>setCharaImage(image,setCurrentSisImage)} />
+                        ))}
+                        {girlImageList.map((image,index)=>(
+                            <Image alt="" key={index} src={image} width={deviceSize.width/7} height={70} onClick={()=>setCharaImage(image,setCurrentGirlImage)} />
+                        ))}
+                        {battleGodImageList.map((image,index)=>(
+                            <Image alt="" key={index} src={image} width={deviceSize.width/7} height={70} onClick={()=>setCharaImage(image,setCurrentGodImage)} />
+                        ))} */}
+                    </div>
+                    キャラクター設定UI
+                    <button onClick={handleCapture} className="px-4 py-2 bg-blue-500 text-white rounded">
+                        撮影
+                    </button>
+                </div>
             </div>
         </>
     );
