77db7f288a46d2a09fa61fc201b909c2f9f62913
MasatomoIde <pakkuku1@gmail.com>
2024-12-06 18:13:09 +0900

    UIupdate

--- app/arPhoto/page.tsx
+++ app/arPhoto/page.tsx
@@ -1,43 +1,44 @@
 "use client";
+import Image from "next/image";
 import NavBar from "../features/common/Navbar/Navbar";
 import { useRef,useEffect,useState } from "react";
 export default function ArPhoto() {
     const canvasRef = useRef<HTMLCanvasElement>(null);
     const videoRef = useRef<HTMLVideoElement>(null);
-    // const [currentGirlImage,setCurrentGirlImage] = useState<string | null>(null);
-    // const [currentBroImage,setCurrentBroImage] = useState<string | null>(null);
-    // const [currentSisImage,setCurrentSisImage] = useState<string | null>(null);
-    // const [currentGodImage,setCurrentGodImage] = useState<string | null>(null);
+    const [currentGirlImage,setCurrentGirlImage] = useState<string | null>(null);
+    const [currentBroImage,setCurrentBroImage] = useState<string | null>(null);
+    const [currentSisImage,setCurrentSisImage] = useState<string | null>(null);
+    const [currentGodImage,setCurrentGodImage] = useState<string | null>(null);
     const [deviceSize,setDeviceSize] = useState<{width:number,height:number}>({width:0,height:0});
-    // // // キャラクター画像のリスト
-    // const girlImageList : string[] = [
-    //     "/images/girl/normal.png",
-    //     "/images/girl/normalWithOp.png",
-    //     "/images/girl/puku.png",
-    //     "/images/girl/winkWithOp.png",
-    //     "/images/girl/winkWithCl.png"
-    // ];
-    // const brotherBirdImageList : string[] = [
-    //     "/images/brotherBird/normal.png",
-    //     "/images/brotherBird/cool.png",
-    //     "/images/brotherBird/shiny.png"
-    // ];
-    // const sisterBirdImageList : string[] = [
-    //     "/images/sisterBird/open.png",
-    //     "/images/sisterBird/close.png",
-    //     "/images/sisterBird/shock.png",
-    //     "/images/sisterBird/smile.png"
-    // ];
-    // const battleGodImageList : string[] = [
-    //     "/images/battleGod/normal.PNG",
-    //     "/images/battleGod/normalOura.PNG",
-    //     "/images/battleGod/normalFull.PNG",
-    //     "/images/battleGod/normalRock.PNG",
-    //     "/images/battleGod/smile.PNG",
-    //     "/images/battleGod/smileOura.PNG",
-    //     "/images/battleGod/smileFull.PNG",
-    //     "/images/battleGod/smileRock.PNG",
-    // ];
+    // キャラクター画像のリスト
+    const girlImageList : string[] = [
+        "/images/girl/normal.png",
+        "/images/girl/normalWithOp.png",
+        "/images/girl/puku.png",
+        "/images/girl/winkWithOp.png",
+        "/images/girl/winkWithCl.png"
+    ];
+    const brotherBirdImageList : string[] = [
+        "/images/brotherBird/normal.png",
+        "/images/brotherBird/cool.png",
+        "/images/brotherBird/shiny.png"
+    ];
+    const sisterBirdImageList : string[] = [
+        "/images/sisterBird/open.png",
+        "/images/sisterBird/close.png",
+        "/images/sisterBird/shock.png",
+        "/images/sisterBird/smile.png"
+    ];
+    const battleGodImageList : string[] = [
+        "/images/battleGod/normal.PNG",
+        "/images/battleGod/normalOura.PNG",
+        "/images/battleGod/normalFull.PNG",
+        "/images/battleGod/normalRock.PNG",
+        "/images/battleGod/smile.PNG",
+        "/images/battleGod/smileOura.PNG",
+        "/images/battleGod/smileFull.PNG",
+        "/images/battleGod/smileRock.PNG",
+    ];
 
     useEffect(()=>{
         setDeviceSize({width:window.innerWidth,height:window.innerHeight});
@@ -81,9 +82,9 @@ export default function ArPhoto() {
         drawFrame();
 
     },[]);
-    // const setCharaImage = (charaImageLink: string,setImageLink:any) => {
-    //     setImageLink(charaImageLink);
-    // }
+    const setCharaImage = (charaImageLink: string,setImageLink:any) => {
+        setImageLink(charaImageLink);
+    }
 
     const handleCapture = () => {
         if (canvasRef.current && videoRef.current) {
@@ -105,9 +106,22 @@ export default function ArPhoto() {
                 {/* スマホの縦、横サイズのキャンバスを作成 */}
                 <video ref={videoRef} autoPlay={true} playsInline={true} muted={true} style={{ display: "none" }} />
                 <canvas ref={canvasRef} width={deviceSize.width} height={deviceSize.height} />
-                <div className="editingMenu fixed bottom-0 left-0 right-0 p-4 flex justify-center space-x-4">
+                <div className="editingMenu fixed bottom-0 left-0 right-0 p-4 flex flex-row justify-center space-x-4">
                     {/* キャラクター設定UIをここに追加 */}
-                    <div className="charactorSettingUI"></div>
+                    <div className="charactorSettingUI">
+                        {brotherBirdImageList.map((image,index)=>(
+                            <Image alt="" key={index} src={image} width={deviceSize.width/7} onClick={()=>setCharaImage(image,setCurrentBroImage)} />
+                        ))}
+                        {sisterBirdImageList.map((image,index)=>(
+                            <Image alt="" key={index} src={image} width={deviceSize.width/7} onClick={()=>setCharaImage(image,setCurrentSisImage)} />
+                        ))}
+                        {girlImageList.map((image,index)=>(
+                            <Image alt="" key={index} src={image} width={deviceSize.width/7} onClick={()=>setCharaImage(image,setCurrentGirlImage)} />
+                        ))}
+                        {battleGodImageList.map((image,index)=>(
+                            <Image alt="" key={index} src={image} width={deviceSize.width/7} onClick={()=>setCharaImage(image,setCurrentGodImage)} />
+                        ))}
+                    </div>
                     キャラクター設定UI
                     <button onClick={handleCapture} className="px-4 py-2 bg-blue-500 text-white rounded">
                         撮影
