cc9d33141807bc079c5bf5644ca00594cf9f2b73
MasatomoIde <pakkuku1@gmail.com>
2024-12-08 04:06:29 +0900

    fれ

--- app/arPhoto/page.tsx
+++ app/arPhoto/page.tsx
@@ -1,6 +1,6 @@
 "use client";
 // import Image from "next/image";
-import NavBar from "../features/common/Navbar/Navbar";
+// import NavBar from "../features/common/Navbar/Navbar";
 import { useRef, useEffect,useState} from "react";
 export default function ArPhoto() {
     const canvasRef_video = useRef<HTMLCanvasElement>(null);
